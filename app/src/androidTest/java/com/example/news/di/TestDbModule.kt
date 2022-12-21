f40cb7286ccecd5a5b13baba78175f24d3797ff3
Joevincent <jvincent1703@gmail.com>
2022-12-21 17:20:06 +0700

    hilt instrument setup

---
+++ app/src/androidTest/java/com/example/news/di/TestDbModule.kt
@@ -0,0 +1,23 @@
+package com.example.news.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.news.core.data.local.ArticleDatabase
+import com.example.news.core.di.app.DbModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+
+@Module
+@TestInstallIn(components = [SingletonComponent::class], replaces = [DbModule::class])
+object TestDbModule {
+
+    @Provides
+    fun provideInMemoryTodoDb(@ApplicationContext context: Context): ArticleDatabase {
+        return Room.inMemoryDatabaseBuilder(context, ArticleDatabase::class.java)
+            .allowMainThreadQueries().build()
+    }
+
+}
